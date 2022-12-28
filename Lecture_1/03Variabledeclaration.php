8d0b7359d8e4bad7d3e948416b5b59d063285f85
yash dodiya <yashsinhrajput1@gmail.com>
2022-12-28 11:50:52 +0530

    Variable_dec

---
+++ Lecture_1/03Variabledeclaration.php
@@ -0,0 +1,11 @@
+<?php
+// comment
+
+$username   ="value";        //correct
+$username123="value";        //correct
+
+// $user name="value";       //wrong
+// $123username="value";     //wrong
+
+echo "data";
+?>
