ae67e8a5f00ea21ed9cf4b7c5e7a1bdf9d99d321
yash dodiya <yashsinhrajput1@gmail.com>
2022-12-28 10:38:26 +0530

    Add

---
+++ Lecture_1/01introduction.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>introduction</title>
+</head>
+<body>
+    <ul>
+        <li> php is server side scripting language</li>
+        <li> php hypertext pre-processor</li>
+        <li>PHP earlier its called as Personal Home Page</li>
+        <li>PHP Rusmus ledorf is inventor/father of PHP</li>
+        <li>PHP 1994 invented 1995 published</li>
+        <li>andi gutmans and zeev suraski invented the ZEND engine</li>
+        <li>PHP 2004-2005 OOPs concets </li>
+        <li>main straingth of PHP is its Frameworks and CMS</li>
+        <li>for access local server we are going to adopt XAMPP (cross plateform, Apache, MariaDB, PHP, Pearl)</li>
+        <li>all data needs to be stored in xampp/htdocs only </li>
+        <li>for access that data http://localhost/laravel/01Dec_laravel_TTS9/</li>
+        <li>PHP is loosly type lang (variable dont needs to be declared before its use) with $ sign we are able to declare variable without any data type</li>
+        <li>echo/print will helps to give plain text response to the client from the server</li>
+        <li>print_r(), var_dump(), var_export() developer option for print array</li>
+    </ul>
+    
+    <h3>Frameworks</h3>
+    <ol>
+        <li>Laravel MIT License</li>
+        <li> CodeIgniter MIT License</li>
+        <li> Symfony MIT License</li>
+        <li> CakePHP MIT License</li>
+        <li> Yii BSD licenses</li>
+        <li> Phalcon BSD licenses</li>
+        <li>Laminas BSD licenses</li>
+        <li>FuelPHP MIT License</li>
+    </ol>
+    <h3>CMS (Context Management System)</h3>
+    <ol>
+        <li>WordPress.</li>
+        <li>Joomla.</li>
+        <li>Drupal.</li>
+        <li>OctoberCMS.</li>
+        <li>OpenCart.</li>
+        <li>ExpressionEngine.</li>
+        <li>PyroCMS.</li>
+        <li>Magento.</li>
+    </ol>
+</body>   
+</html>
