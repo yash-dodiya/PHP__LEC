008835aa2d9bec9c64167f52fd28735b202be7d3
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-01 23:12:06 +0530

    Switch_case

---
+++ Switchcase.php
@@ -0,0 +1,33 @@
+<?php
+// error_reporting(0);
+$weekday=;
+
+switch($weekday)
+{
+    case 1:
+    echo "Today is monday:";
+    break;
+    case 2:
+    echo "Today is tuesday:";
+    break;
+    case 3:
+    echo "Today is wendsday";
+    break;
+    case 4:
+    echo "Today is thursday:";
+    break;
+    case 5:
+    echo "Today is friday:";
+    break;
+    case 6:
+    echo "Today is saturday:";
+    break;
+    case 7:
+    echo "Today is sunday:";
+    break;
+    default:
+    
+        echo "enter correct weekday";
+    
+}
+?>
