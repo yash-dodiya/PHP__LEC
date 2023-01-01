97f925d7163c085dec378ca2cd5beb9aa8e161aa
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-01 11:00:19 +0530

    Pattern.php

---
+++ Lecture_1/07Pattern.php
@@ -0,0 +1,18 @@
+<?php
+
+for($a=1;$a<=5;$a++)
+{
+      // echo "a is $a";
+  
+   for($j=1;$j<=$a;$j++)
+   {
+      // echo "j is $j";
+    
+      echo " * ";
+   }
+
+      echo "<br>";
+}
+
+
+?>
