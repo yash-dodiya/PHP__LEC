7f8174e8c511aa4a1f4ee4b44defff3d683afe42
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-01 14:58:46 +0530

    TernoryOp.php

---
+++ Lecture_1/08TernoryOp.php
@@ -0,0 +1,18 @@
+<?php
+$marks=50;
+// $marks="null";
+
+if($marks>50)
+{
+    echo "pass";
+}
+else
+{
+    echo "try again";
+}
+
+
+// echo $marks>=50 ? "pass:" : "try again";
+
+echo "<br>";
+?>
