f8273d322a6ac897acc4d75a41fb751c08e12994
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-06 11:31:51 +0530

    Foreach_loop

---
+++ Lecture_2/Foreachloop.php
@@ -0,0 +1,22 @@
+<?php
+
+// $name=["yash",
+//       "milan",
+//       "nihar",
+//       "ravi",
+//       "shakti" ];
+
+// foreach ($name as $value) {
+//     echo "$value <br>";
+// }      
+
+$age=["yash"=>18,
+      "ravi"=>15,
+      "nihar"=>14,
+      "sakti"=>13];
+
+    foreach($age as$key=>$value)
+    {
+        echo "$key=$value.<br>";
+    }
+?>
