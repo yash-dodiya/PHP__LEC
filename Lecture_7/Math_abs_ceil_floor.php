c1d71e9accbec950cf724cd8e35585d5aab87a63
yash dodiya <yashsinhrajput1@gmail.com>
2023-01-24 16:38:51 +0530

    Maths_logic

---
+++ Lecture_7/Math_abs_ceil_floor.php
@@ -0,0 +1,41 @@
+<?php
+
+echo "<br>========================================echo_ceil========================================</br>";
+echo (ceil(0.60)."<br>");
+echo (ceil(0.40)."<br>");
+echo (ceil(1.60)."<br>");
+echo (ceil(5.90)."<br>");
+echo (ceil(-5.6)."<br>");
+echo (ceil(-5.9)."<br>");
+
+
+echo "<br>========================================echo_floor========================================</br>";
+echo (floor(0.60)."<br>");
+echo (floor(0.40)."<br>");
+echo (floor(1.60)."<br>");
+echo (floor(5.60)."<br>");
+echo (floor(-5.60)."<br>");
+echo (floor(-5.2)."<br>");
+echo (floor(4.60)."<br>");
+
+
+echo "<br>========================================echo_round========================================</br>";
+echo (round(0.60)."<br>");
+echo (round(1.60)."<br>");
+echo (round(5.90)."<br>");
+echo (round(-1.60)."<br>");
+echo (round(-5.60)."<br>");
+echo (round(7.60)."<br>");
+echo (round(4.40)."<br>");
+echo (round(99.60)."<br>");
+
+
+echo "<br>========================================echo_abs========================================</br>";
+echo (abs(-0.60)."<br>");
+echo (abs(0.40)."<br>");
+echo (abs(1.60)."<br>");
+echo (abs(7.60)."<br>");
+echo (abs(-5.60)."<br>");
+echo (abs(-5.90)."<br>");
+echo (abs(9.60)."<br>");
+?>
---
+++ Lecture_7/Math_intdiv_sqrt_pow.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "<br>====================================Math_integer/division==================================</br>";
+echo intdiv(8,4)."<br>";
+echo intdiv(100,2)."<br>";
+echo intdiv(-4,-2)."<br>";
+echo intdiv(40,-3)."<br>";
+echo intdiv(24,4)."<br>";
+
+echo "<br>===================================Math_squarroot==========================================</br>";
+echo sqrt(4)."<br>";
+echo sqrt(16)."<br>";
+echo sqrt(625)."<br>";
+echo sqrt(36)."<br>";
+echo sqrt(-81)."<br>";
+
+echo "<br>===================================Math_pow================================================</br>";
+echo pow(8,2)."<br>";
+echo pow(462)."<br>";
+echo pow(7,3)."<br>";
+echo pow(2,4)."<br>";
+echo pow(10,3)."<br>";
+
+
+?>
