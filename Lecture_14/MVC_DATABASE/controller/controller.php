cb612f3c0f603b80f6526b027dc386d57efce624
yash dodiya <yashsinhrajput1@gmail.com>
2023-02-06 11:56:15 +0530

    Add

--- Lecture_14/MVC_DATABASE/controller/controller.php
+++ Lecture_14/MVC_DATABASE/controller/controller.php
@@ -1,8 +1,18 @@
 <?php
 
 
-class controller{
+    class controller{
+        public $base_url="";
         public function __construct(){
+            // echo "<pre>";
+            // print_r($_SERVER);
+            $requrlData=explode("/",$_SERVER['REQUEST_URI']);
+            // print_r($requrlData);
+            $this->base_url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].
+            "/".$requrlData[1]."/".$requrlData[2]."/".$requrlData[3]."/".$requrlData[4]."/assets/";
+            // echo "static base_url : http://localhost/PHP___LEC/Lecture_13/MVC_DY/assets/<br>";
+            // echo "Dynemic base_url : .$this->base_url";
+            // exit;
             if(isset($_SERVER["PATH_INFO"])){
                 switch ($_SERVER["PATH_INFO"]) {
                     case '/home':
@@ -57,6 +67,15 @@ class controller{
                         include_once("views/header.php");
                         include_once("views/404.php");
                         include_once("views/footer.php");
+                        break;
+
+                    case '/login':
+                        include_once("views/login_regis.php");
+                        if(isset($_POST['register'])){
+
+                            echo "<pre>";
+                            print_r($_REQUEST);
+                        }
                         break;
 
                     default:
---
+++ Lecture_14/MVC_DATABASE/views/courses.php
@@ -0,0 +1,69 @@
+<!-- Courses Start -->
+    <div class="container-xxl py-6">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <h6 class="text-primary text-uppercase mb-2">Tranding Courses</h6>
+                <h1 class="display-6 mb-4">Our Courses Upskill You With Driving Training</h1>
+            </div>
+            <div class="row g-4 justify-content-center">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="courses-item d-flex flex-column bg-light overflow-hidden h-100">
+                        <div class="text-center p-4 pt-0">
+                            <div class="d-inline-block bg-primary text-white fs-5 py-1 px-4 mb-4">$99</div>
+                            <h5 class="mb-3">Automatic Car Lessons</h5>
+                            <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
+                            <ol class="breadcrumb justify-content-center mb-0">
+                                <li class="breadcrumb-item small"><i class="fa fa-signal text-primary me-2"></i>Beginner</li>
+                                <li class="breadcrumb-item small"><i class="fa fa-calendar-alt text-primary me-2"></i>3 Week</li>
+                            </ol>
+                        </div>
+                        <div class="position-relative mt-auto">
+                            <img class="img-fluid" src="assets/courses-1.jpg" alt="">
+                            <div class="courses-overlay">
+                                <a class="btn btn-outline-primary border-2" href="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="courses-item d-flex flex-column bg-light overflow-hidden h-100">
+                        <div class="text-center p-4 pt-0">
+                            <div class="d-inline-block bg-primary text-white fs-5 py-1 px-4 mb-4">$99</div>
+                            <h5 class="mb-3">Highway Driving Lesson</h5>
+                            <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
+                            <ol class="breadcrumb justify-content-center mb-0">
+                                <li class="breadcrumb-item small"><i class="fa fa-signal text-primary me-2"></i>Beginner</li>
+                                <li class="breadcrumb-item small"><i class="fa fa-calendar-alt text-primary me-2"></i>3 Week</li>
+                            </ol>
+                        </div>
+                        <div class="position-relative mt-auto">
+                            <img class="img-fluid" src="assets/courses-2.jpg" alt="">
+                            <div class="courses-overlay">
+                                <a class="btn btn-outline-primary border-2" href="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="courses-item d-flex flex-column bg-light overflow-hidden h-100">
+                        <div class="text-center p-4 pt-0">
+                            <div class="d-inline-block bg-primary text-white fs-5 py-1 px-4 mb-4">$99</div>
+                            <h5 class="mb-3">International Driving</h5>
+                            <p>Tempor erat elitr rebum at clita dolor diam ipsum sit diam amet diam et eos</p>
+                            <ol class="breadcrumb justify-content-center mb-0">
+                                <li class="breadcrumb-item small"><i class="fa fa-signal text-primary me-2"></i>Beginner</li>
+                                <li class="breadcrumb-item small"><i class="fa fa-calendar-alt text-primary me-2"></i>3 Week</li>
+                            </ol>
+                        </div>
+                        <div class="position-relative mt-auto">
+                            <img class="img-fluid" src="assets/courses-3.jpg" alt="">
+                            <div class="courses-overlay">
+                                <a class="btn btn-outline-primary border-2" href="">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Courses End -->
